40b72cd8bb446176a8fbe6f7a10e03ab17cf7557
Ionică Bizău <bizauionica@yahoo.com>
2013-10-18 13:52:19 +0300

    Comments

--- main.cpp
+++ main.cpp
@@ -17,21 +17,32 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    // build the web view
     QWebView *view = new QWebView();
 
-    QString HTML_PATH       = argv[1];
+    // get the html path
+    QString HTML_PATH       = QString(argv[1]);
 
-    if (!HTML_PATH.startsWith("htt")) {
-        HTML_PATH = "file://" + QDir::current().absolutePath() + QDir::separator() + QString(argv[1]);
+    // the path is NOT a web site url
+    if (!HTML_PATH.startsWith("htt"))
+    {
+        // get it from the local machine
+        HTML_PATH = "file://" + QDir::current().absolutePath() + QDir::separator() + HTML_PATH;
     }
 
+    // set window width and height
     int     WINDOW_WIDTH    = QString(argv[2]).toInt();
     int     WINDOW_HEIGHT   = QString(argv[3]).toInt();
     // TODO decoration
 
-    qDebug() << HTML_PATH;
+    // resize the window
     view->resize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    // load that HTML file or website
     view->load(QUrl(HTML_PATH));
+
+    // show the web view
     view->show();
 
     return app.exec();
