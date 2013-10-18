8f4a4aff9c2b366a3b61906485bfbb540acbd5db
Ionică Bizău <bizauionica@yahoo.com>
2013-10-18 11:07:51 +0300

    Documentation, load HTML paths
     - added small documentation (utlity)
     - added the feature to load a HTML file from the local machine (the first argument)
     - set the sizes of the window

--- main.cpp
+++ main.cpp
@@ -1,14 +1,37 @@
 #include "mainwindow.h"
-#include <QApplication>
 #include <QWebView>
+#include <QtWidgets>
+#include <QWebFrame>
+#include <QDir>
 
+/*
+ *  Utility:
+ *  ./exectuable pageAddress width height decoration
+ *
+ *   - pageAddress: the html file path or url
+ *   - width:       integer that represent the width of the window
+ *   - height:      integer that represent the height of the window
+ *   - decoration:  one of the following values: "..." (TODO)
+ *
+ * */
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QWebView *view = new QWebView();
 
-    view->resize(400,500);
-    view->load(QUrl("http://m.facebook.com"));
+    QString HTML_PATH       = argv[1];
+
+    if (!HTML_PATH.startsWith("htt")) {
+        HTML_PATH = "file://" + QDir::current().absolutePath() + QDir::separator() + QString(argv[1]);
+    }
+
+    int     WINDOW_WIDTH    = QString(argv[2]).toInt();
+    int     WINDOW_HEIGHT   = QString(argv[3]).toInt();
+    // TODO decoration
+
+    qDebug() << HTML_PATH;
+    view->resize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    view->load(QUrl(HTML_PATH));
     view->show();
 
     return app.exec();
