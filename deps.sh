9e02b1a7ccfc94c4634114fbc190d58402250965
Ionică Bizău <bizauionica@yahoo.com>
2013-12-05 22:03:35 +0200

    More deps.

--- deps.sh
+++ deps.sh
@@ -3,3 +3,6 @@ sudo apt-get install qt5-default
 
 # Project ERROR: Unknown module(s) in QT: webkitwidgets webkit
 sudo apt-get install libqt5webkit5-dev
+
+# Project ERROR: Unknown module(s) in QT: quick location sensors
+sudo apt-get install qtquick1-5-dev qtlocation5-dev qtsensors5-dev qtdeclarative5-dev
