8dbcdbed020197045fe4732e1fc74935cb820e72
Ionică Bizău <bizauionica@yahoo.com>
2013-12-05 22:00:11 +0200

    libqt5webkit5-dev dep

--- deps.sh
+++ deps.sh
@@ -1,2 +1,5 @@
 echo "This script will install the dependencies to be able to compile the project."
 sudo apt-get install qt5-default
+
+# Project ERROR: Unknown module(s) in QT: webkitwidgets webkit
+sudo apt-get install libqt5webkit5-dev
