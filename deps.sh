9c0c3339bc6f462d6e7e01b44176354d52a3f1af
Ionică Bizău <bizauionica@yahoo.com>
2013-12-07 19:27:32 +0200

    install node

--- deps.sh
+++ deps.sh
@@ -1,6 +1,9 @@
 echo "This script will install the dependencies to be able to compile the project."
+
+# install qt5-default
 sudo apt-get install qt5-default
 
+### Solving compiling errors
 # Project ERROR: Unknown module(s) in QT: webkitwidgets webkit
 sudo apt-get install libqt5webkit5-dev
 
@@ -9,3 +12,10 @@ sudo apt-get install qtquick1-5-dev qtlocation5-dev qtsensors5-dev qtdeclarative
 
 # /usr/bin/ld: cannot find -lsqlite3
 sudo apt-get install libsqlite3-dev
+
+### Install node
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
