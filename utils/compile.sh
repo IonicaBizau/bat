34aae50e1cb25d067832fac7c09b4076c252088a
Ionică Bizău <bizauionica@yahoo.com>
2013-12-08 09:56:37 +0200

    Moved shell scripts in utils/ directory

--- utils/compile.sh
+++ utils/compile.sh
@@ -1,5 +1,10 @@
 echo "Compiling ..."
 
+# TODO Avoid using `cd`
+
+# go to main directory
+# cd ..
+
 # go to source directory
 cd source
 
@@ -8,6 +13,9 @@ qmake JohnnysWebView.pro
 
 # make file
 qmake -makefile
+
+# make
+make
 
 # go back to the main directory
 cd ..
