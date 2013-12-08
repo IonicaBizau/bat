b06781169f5ea8f4b4e196197f71a4d50be13731
Ionică Bizău <bizauionica@yahoo.com>
2013-12-08 10:06:02 +0200

    Deb build for 64bit.

--- utils/deb-build.sh
+++ utils/deb-build.sh
@@ -1,8 +1,10 @@
-echo "> Starting ..."
 echo "> Copying the executable file into deb directory"
-cp build-JohnnysWebView-Desktop_Qt_5_1_1_GCC_32bit-Debug/JohnnysWebView ./deb/32bit/usr/bin/johnnys-webview
-echo "> Removing all deb files from ./deb/32bit directory."
-rm deb/32bit/*.deb
+cp source/JohnnysWebView ./deb/64bit/usr/bin/johnnys-webview
+
+echo "> Removing all deb files from ./deb/64bit directory."
+rm deb/64bit/*.deb
+
 echo "> Building deb file ..."
-dpkg -b deb/32bit/ deb/32bit/32-dev-release.deb
+dpkg -b deb/64bit/ deb/64bit/64-dev-release.deb
+
 echo " * Finished."
