9739053bc47212bf2852446a7a22ca91aa8d8030
Ionică Bizău <bizauionica@yahoo.com>
2014-01-16 16:37:00 +0200

    Fixed #7. Added the client script that creates a pseudo api object.

---
+++ client/pseudo-api.js
@@ -0,0 +1,71 @@
+(function (window) {
+
+    // do nothing if the page was loaded in 
+    // johnny's webview app
+    if (typeof $API !== "undefined") {
+        return;
+    }
+
+    var debugMode = false
+
+    // export $API object
+    window["$API"] = {
+        closeWindow: function () {
+        },
+        resize: function (width, height) {
+        },
+        setWindowFlags: function (type) {
+        },
+        setWindowState: function (type) {
+        },
+        getWindowSize: function () {
+            return {
+                width: 200,
+                height: 200
+            };
+        },
+        getScreenSize: function () {
+            return {
+                width: 400,
+                height: 400
+            };
+        },
+        setWindowPosition: function (left, top) {
+        },
+        getWindowPosition: function () {
+
+            return {
+                left: 10,
+                top: 10
+            };
+        },
+        getMousePosition: function () {
+            return {
+                left: 30,
+                top: 10
+            };
+        },
+        setMousePosition: function (x, y) {
+        },
+        setWindowTitle: function (newTitle) {
+            document.title = newTitle;
+        },
+        writeFile: function (filePath, content) {
+        },
+        readFile: function (path) {
+        },
+        debug: function (message) {
+            console.debug(message);
+        },
+        inspectElement: function () {
+        },
+        runBash: function (command) {
+        },
+        setDebugMode: function (debug) {
+            debugMode = debug;
+        },
+        getDebugMode: function () {
+            return debugMode;
+        }
+    };
+})(window);
