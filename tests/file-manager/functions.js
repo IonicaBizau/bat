4138b254d0974eaada7f54b4b00e06964bd932b0
Ionică Bizău <bizauionica@yahoo.com>
2014-01-23 10:58:07 +0200

    Added font-aswesome and some CSS.

--- tests/file-manager/functions.js
+++ tests/file-manager/functions.js
@@ -1,27 +1,47 @@
 $(document).ready(function () {
     var $files = $(".files");
 
-    readDir("/");
+    var $templates = {
+        "file": $(".template.file").clone().removeClass("template"),
+        "folder": $(".template.folder").clone().removeClass("template")
+    };
 
-    $files.on("click", "a", function () {
-        readDir($(this).attr("href"));
+    $(".template.file").remove();
+    $(".template.folder").remove();
+
+    $files.on("dblclick", "[data-path]", function () {
+        readDir($(this).attr("data-path"));
         return false;
     });
 
-    function readDir (cDir) {
+
+    window.readDir = function readDir (cDir) {
         $files.empty()
-        var commandRes = $API.runBash("ls " + cDir);
+        var commandRes = $API.runBash("ls " + cDir + " -F --group-directories-first");
         if (commandRes.stderr) {
             $files.html(commandRes.stderr);
         }
         var filesAndDirs = getFilesAndDirs(commandRes.stdout);
         for (var i in filesAndDirs) {
-            var $a = $("<a>")
-                        .attr("href", cDir + "/" + filesAndDirs[i])
-                        .html(filesAndDirs[i]);
-            $files.append($("<li>").append($a));
+            var cFileOrDir = filesAndDirs[i];
+            var isFolder = cFileOrDir.slice(-1) === "/" ? true : false;
+            
+            if (isFolder) {
+                cFileOrDir = cFileOrDir.substr(0, cFileOrDir.length - 1);
+            }
+
+            var $template = $templates[isFolder ? "folder" : "file"];
+
+            var $newFileOrDir = $template
+                                    .clone()
+                                    .attr("data-path", cDir + filesAndDirs[i])
+            $newFileOrDir.find(".name").text(cFileOrDir)
+
+            $files.append($newFileOrDir);
         }
     }
+    
+    readDir("/");
 
     function getFilesAndDirs (output) {
         return output.split("\n");
