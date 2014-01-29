98d3ff2588948d553173b5d2a0fd0143831babee
Ionică Bizău <bizauionica@yahoo.com>
2014-01-29 07:59:23 +0200

    Show some output.

--- utils/compile.sh
+++ utils/compile.sh
@@ -6,21 +6,27 @@ echo "Compiling ..."
 # cd ..
 
 # go to source directory
+echo "> Entering in the source directory ..."
 cd source
 
 # qmake
+echo "> Qmaking JohnnysWebView.pro ..."
 qmake JohnnysWebView.pro
 
 # make file
+echo "> Qmaking makefile ..."
 qmake -makefile
 
 # make
+echo "> Make ..."
 make
 
 # go back to the main directory
+echo "> Entering in johnnys-webview root ..."
 cd ..
 
 # TODO Should this made always?
+echo "> Updating the johnnys-webview application in /usr/bin. Password may be required."
 sudo cp source/JohnnysWebView /usr/bin/johnnys-webview
 
 # TODO Generate deb packages and push them
